dddf56fbf650078de126e5be3e5b05fc756fa2c6
Keeven Oliveira <66172398+ProgKeeven@users.noreply.github.com>
2021-02-09 17:24:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const linkRoute = require('./routes/linkRoute')
 
 mongoose.connect('mongodb://localhost/newlinks', { useUnifiedTopology: true, useNewUrlParser: true,useFindAndModify: false });
 
-app.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
-});
-
 let db = mongoose.connection;
 
 db.on("error", () => { console.log("houve um erro") });
